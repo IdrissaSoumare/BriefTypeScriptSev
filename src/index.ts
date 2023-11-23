d362a08a832c50168cc2b1fed84335a770ee3d82
IdrissaSoumare <soumare.id@laposte.net>
2023-11-23 08:55:33 +0100

    derniere rectif

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@
 import { Hero } from "./hero";
 
 //instanciation des 2 objets, soit : vegeta et sangoku
-const vegeta = new Hero("vegeta", 10, 60);
+const vegeta = new Hero("vegeta", 10, 50);
 const sangoku = new Hero("sangoku", 10, 50);
+
+
 
 // boucle où les 2 combattants s'affrontent et le vainqueur s'affiche en fontion de sa puissance et de ses vies restantes.
 while (sangoku.isAlive() && vegeta.isAlive()) {
@@ -14,7 +16,7 @@ while (sangoku.isAlive() && vegeta.isAlive()) {
         console.log("vegeta is the winner");
     } else if (sangoku.isAlive() && !vegeta.isAlive()) {
         console.log("sangoku is the winner");
-    } else {
+    } else if (!sangoku.isAlive() && !vegeta.isAlive()) {
         console.log("It's a draw");
     }
 }
